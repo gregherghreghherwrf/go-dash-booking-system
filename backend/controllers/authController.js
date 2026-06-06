81b3bc743f090c8ea08f95c6f28436409e1718d8
Harsh Khatri <harsh.khatri2105@email.com>
2026-06-06 16:15:50 +0530

    Add loginUser controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,3 +64,31 @@ async(req,res)=>{
  });
 
 };
+
+exports.loginUser = async (req, res) => {
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (
+    user &&
+    (await bcrypt.compare(password, user.password))
+  ) {
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+
+  } else {
+
+    res.status(401).json({
+      message: "Invalid email or password"
+    });
+
+  }
+
+};
