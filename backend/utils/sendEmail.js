3619c6164d4e436c4a48bee9d1a8cb1d688fb8ba
Harsh Khatri <harsh.khatri2105@email.com>
2026-06-10 13:29:18 +0530

    Fix email notifications

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,38 +1,26 @@
-const nodemailer =
-require("nodemailer");
+const nodemailer = require("nodemailer");
 
-const sendEmail =
-async(email,subject,text)=>{
+const sendEmail = async (email, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
- const transporter =
- nodemailer.createTransport({
+    await transporter.sendMail({
+      from: `"Go Dash Sports" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject,
+      text,
+    });
 
-  service:"gmail",
-
-  auth:{
-   user:
-   process.env.EMAIL_USER,
-
-   pass:
-   process.env.EMAIL_PASS
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Email Error:", error.message);
   }
-
- });
-
- await transporter.sendMail({
-
-  from:
-  process.env.EMAIL_USER,
-
-  to:email,
-
-  subject,
-
-  text
-
- });
-
 };
 
-module.exports =
-sendEmail;
+module.exports = sendEmail;
